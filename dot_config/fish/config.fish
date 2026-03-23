a6f9bacc96a234cf5968428bf0fd236e86b19320
Rizky Dana <rizkydana14@gmail.com>
2026-03-23 09:03:04 +0800

    dot_config: add fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+zoxide init fish | source
+
+abbr --add bw bunx bun-workspaces
+abbr --add oc opencode
+abbr --add brd bun run dev
